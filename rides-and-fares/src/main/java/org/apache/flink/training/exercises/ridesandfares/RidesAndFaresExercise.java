b881b7249675ab80934c55a48d7c3555d4635059
Kai <kai@edgesix.com>
2023-04-18 15:43:00 -0300

    Solutions for ride-cleansing and rides-and-fares

--- rides-and-fares/src/main/java/org/apache/flink/training/exercises/ridesandfares/RidesAndFaresExercise.java
+++ rides-and-fares/src/main/java/org/apache/flink/training/exercises/ridesandfares/RidesAndFaresExercise.java
@@ -18,6 +18,8 @@
 
 package org.apache.flink.training.exercises.ridesandfares;
 
+import java.io.IOException;
+
 import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -31,8 +33,11 @@ import org.apache.flink.training.exercises.common.datatypes.TaxiFare;
 import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
 import org.apache.flink.training.exercises.common.sources.TaxiFareGenerator;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
-import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 import org.apache.flink.util.Collector;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 
 /**
  * The Stateful Enrichment exercise from the Flink training.
@@ -99,19 +104,40 @@ public class RidesAndFaresExercise {
     public static class EnrichmentFunction
             extends RichCoFlatMapFunction<TaxiRide, TaxiFare, RideAndFare> {
 
+    	private transient ValueState<TaxiRide> rides = null;
+    	private transient ValueState<TaxiFare> fares = null;
+    	
         @Override
         public void open(Configuration config) throws Exception {
-            throw new MissingSolutionException();
+        	 ValueStateDescriptor<TaxiRide> ridesDescriptor =
+                     new ValueStateDescriptor<>("rides", TypeInformation.of(new TypeHint<TaxiRide>() {}));
+             this.rides = getRuntimeContext().getState(ridesDescriptor);
+        	 ValueStateDescriptor<TaxiFare> faresDescriptor =
+                     new ValueStateDescriptor<>("fares", TypeInformation.of(new TypeHint<TaxiFare>() {}));
+             this.fares = getRuntimeContext().getState(faresDescriptor);
         }
+
+        private void addRideAndFareIfBothAvailable(Collector<RideAndFare> out) {
+        	try {
+				if (this.rides.value() != null && this.fares.value() != null) {
+					out.collect(new RideAndFare(this.rides.value(), this.fares.value()));
+					this.rides.clear();
+					this.fares.clear();
+				}
+			} catch (IOException e) {
+			}
+		}
 
         @Override
         public void flatMap1(TaxiRide ride, Collector<RideAndFare> out) throws Exception {
-            throw new MissingSolutionException();
+        	this.rides.update(ride);
+        	this.addRideAndFareIfBothAvailable(out);
         }
 
-        @Override
+		@Override
         public void flatMap2(TaxiFare fare, Collector<RideAndFare> out) throws Exception {
-            throw new MissingSolutionException();
+        	this.fares.update(fare);
+        	this.addRideAndFareIfBothAvailable(out);    	
         }
     }
 }
